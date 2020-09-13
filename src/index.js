3c132855488e87a9e6bd31515ea80b3174ed5c9a
NedVill <liamholwett@gmail.com>
2020-09-13 13:29:11 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let totalStr = str;
+
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		let replacedStr = replacer(totalStr, bracketsConfig[i].join(''));
+		
+		if (totalStr !== replacedStr) {
+			totalStr = replacedStr;
+			i = -1;
+		}
+	}
+
+	return totalStr.length > 0 ? false : true;
+
+	function replacer(str, replace) {
+		
+		if (str.length > 0 && str.replace(replace, '') !== str) {
+			return replacer(str.replace(replace, ''), replace);
+		}
+
+		return str
+	}
 }
